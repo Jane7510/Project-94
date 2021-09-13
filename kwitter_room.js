a678dae7c43eb30127045dd2b3baa4d91ae775dd
Jane7510 <80323076+Jane7510@users.noreply.github.com>
2021-09-13 13:16:44 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDyMsYvb8CcToaIONaLc7K4cURVZQ8Y9n8",
+  authDomain: "project-94-b326d.firebaseapp.com",
+  databaseURL: "https://project-94-b326d-default-rtdb.firebaseio.com",
+  projectId: "project-94-b326d",
+  storageBucket: "project-94-b326d.appspot.com",
+  messagingSenderId: "35704807408",
+  appId: "1:35704807408:web:e7cd8eb14d08c60d604735",
+  measurementId: "G-4S31YNZHHY"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
